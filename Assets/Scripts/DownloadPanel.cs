7c65cb7d765545818cbae7949b060650a43044b4
user12061999 <user12061999@gmail.com>
2025-05-05 14:55:07 +0700

    update UI ModCraft_V02

--- Assets/Scripts/DownloadPanel.cs
+++ Assets/Scripts/DownloadPanel.cs
@@ -11,9 +11,19 @@ public class DownloadPanel : MonoBehaviour
     public void SetValue(ModData data)
     {
         gallary.sprite = data.gallary;
-        des.text = data.des;
+        if (des != null)
+        {
+            string processedText = data.des;
+            processedText=ReplaceDoubleSpaceWithNewline(processedText);
+            Debug.Log(processedText);
+            des.text= processedText;
+        }
     }
-
+    string ReplaceDoubleSpaceWithNewline(string input)
+    {
+        input=input.Replace(": ", ":\n");
+        return input.Replace("  ", "\n");
+    }
     private void Start()
     {
         SetValue(GameManager.ins.modData[GameManager.ins.modDataSelection]);
--- Assets/Scripts/ModView.cs
+++ Assets/Scripts/ModView.cs
@@ -8,7 +8,7 @@ using UnityEngine.UI;
 public class ModView : MonoBehaviour
 {
     public int ID;
-    public Image icon;
+    public Image icon,gallary;
     public TextMeshProUGUI txtType,txtName,txtDes,txtPercent,txtDownload,txtView,txtPublished;
 
     private void Start()
@@ -29,16 +29,34 @@ public class ModView : MonoBehaviour
     }
     public void SetValue(ModData data)
     {
-        icon.sprite = data.icon;
-        txtType.text= data.type;
-        txtName.text = data.nameMod;
-        txtDes.text = data.des;
-        txtPercent.text = data.percent;
-        txtDownload.text = data.download;
-        txtView.text = data.view;
-        txtPublished.text = data.published;
-    }
+        if (icon != null) icon.sprite = data.icon;
+        
+        if (gallary != null) gallary.sprite = data.gallary;
+        
+        if (txtType != null) txtType.text = data.type;
+        
+        if (txtName != null) txtName.text = data.nameMod;
 
+        if (txtDes != null)
+        {
+            string processedText = data.des;
+            processedText=ReplaceDoubleSpaceWithNewline(processedText);
+            Debug.Log(processedText);
+            txtDes.text= processedText;
+        }
+
+        if (txtPercent != null) txtPercent.text = data.percent;
+
+        if (txtDownload != null) txtDownload.text = data.download;
+
+        if (txtView != null) txtView.text = data.view;
+
+        if (txtPublished != null) txtPublished.text = data.published;
+    }
+    string ReplaceDoubleSpaceWithNewline(string input)
+    {
+        return input.Replace("  ", "\n");
+    }
     public void OnClickButton()
     {
         GameManager.ins.modDataSelection = ID;
