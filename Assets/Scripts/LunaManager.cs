57dfb355d919ce5ac759ef4aa19d6b10371bd5de
Tiêu Kiếm <107684026+realtieukiem@users.noreply.github.com>
2026-04-16 16:00:19 +0700

    up

--- Assets/Scripts/LunaManager.cs
+++ Assets/Scripts/LunaManager.cs
@@ -38,6 +38,13 @@ public class LunaManager : MonoBehaviour
     [LunaPlaygroundField("Text Position")] public Vector2 positionTextEndCreative;
     [LunaPlaygroundField("Text Size")] public float sizeTextEndCreative;
     [LunaPlaygroundField("Text Style")] public FontStyles fontStyleTextEndCreative;
+    public TextMeshProUGUI txtEndCreative2;
+
+    [LunaPlaygroundField("Text2")] public string stringEndCreative2;
+    [LunaPlaygroundField("Text Color2")] public Color colorTextEndCreative2;
+    [LunaPlaygroundField("Text Position2")] public Vector2 positionTextEndCreative2;
+    [LunaPlaygroundField("Text Size2")] public float sizeTextEndCreative2;
+    [LunaPlaygroundField("Text Style2")] public FontStyles fontStyleTextEndCreative2;
 
     [LunaPlaygroundField("Hand Position")] public Vector2 handPos;
     //----------------------------------LUNA----------------------------
@@ -75,6 +82,12 @@ public class LunaManager : MonoBehaviour
         txtEndCreative.fontSize = sizeTextEndCreative;
         txtEndCreative.fontStyle = fontStyleTextEndCreative;
         txtEndCreative.rectTransform.anchoredPosition = positionTextEndCreative;
+        txtEndCreative2.text = stringEndCreative2;
+        txtEndCreative2.color = colorTextEndCreative2;
+        txtEndCreative2.fontSize = sizeTextEndCreative2;
+        txtEndCreative2.fontStyle = fontStyleTextEndCreative2;
+        txtEndCreative2.rectTransform.anchoredPosition = positionTextEndCreative2;
+
         handTrans.transform.localPosition = handPos;
         video.clip = videoClip;
     }
