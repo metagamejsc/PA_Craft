56de1add00cf9f6b689d4e30a30c62852585496a
HuyNQ <dev@metagamejsc.com>
2026-06-16 09:50:49 +0700

    CBWW2_V03: fix bug

--- Assets/_Core_/Scripts/House.cs
+++ Assets/_Core_/Scripts/House.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class House : MonoBehaviour
@@ -9,6 +8,7 @@ public class House : MonoBehaviour
     [SerializeField] private GameObject _eggs;
     [SerializeField] private AudioClip _dogSound;
     [SerializeField] private AudioClip _birdSound;
+    [SerializeField] private AudioSource _audio;
 
     private void Start()
     {
@@ -22,22 +22,26 @@ public class House : MonoBehaviour
     {
         _dog.SetActive(true);
         PlayAnimalSound(_dogSound);
+        _audio.Play();
     }
 
     public void ShowMeat()
     {
         _meat.SetActive(true);
+        _audio.Play();
     }
 
     public void ShowEggs()
     {
         _eggs.SetActive(true);
+        _audio.Play();
     }
 
     public void ShowBird()
     {
         _bird.SetActive(true);
         PlayAnimalSound(_birdSound);
+        _audio.Play();
     }
 
     private void PlayAnimalSound(AudioClip clip)
