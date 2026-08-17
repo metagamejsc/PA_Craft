3ab8e10205baa4ece49332f12f6898e98c2c2d85
HuyNQ <huy892001dx@gmail.com>
2026-08-17 09:45:26 +0700

    UAC_V73: fix bug

--- Assets/_Playable_/Scripts/GameController.cs
+++ Assets/_Playable_/Scripts/GameController.cs
@@ -11,12 +11,73 @@ namespace Playable
 
         [SerializeField] private float _moveDistanceX = 1f;
         [SerializeField] private float _moveDuration = 1f;
+        [SerializeField] private GameObject _objRank;
 
         private readonly List<Tween> _blockTweens = new List<Tween>();
+        private RectTransform _rankRectTransform;
+        private Vector2 _rankInitialPosition;
+        private Vector2 _rankInitialAnchorMin;
+        private Vector2 _rankInitialAnchorMax;
+        private Vector2Int _lastScreenSize;
 
         private void Start()
         {
+            CacheRankPosition();
+            UpdateRankPosition();
             StartBlockMovement();
+        }
+
+        private void Update()
+        {
+            Vector2Int screenSize = new Vector2Int(Screen.width, Screen.height);
+            if (screenSize == _lastScreenSize)
+            {
+                return;
+            }
+
+            UpdateRankPosition();
+        }
+
+        private void CacheRankPosition()
+        {
+            if (_objRank == null)
+            {
+                return;
+            }
+
+            _rankRectTransform = _objRank.GetComponent<RectTransform>();
+            if (_rankRectTransform != null)
+            {
+                _rankInitialPosition = _rankRectTransform.anchoredPosition;
+                _rankInitialAnchorMin = _rankRectTransform.anchorMin;
+                _rankInitialAnchorMax = _rankRectTransform.anchorMax;
+            }
+        }
+
+        private void UpdateRankPosition()
+        {
+            _lastScreenSize = new Vector2Int(Screen.width, Screen.height);
+
+            if (_rankRectTransform == null)
+            {
+                return;
+            }
+
+            bool isPortrait = Screen.height > Screen.width;
+
+            Vector2 anchorMin = isPortrait
+                ? new Vector2(0.5f, _rankInitialAnchorMin.y)
+                : _rankInitialAnchorMin;
+            Vector2 anchorMax = isPortrait
+                ? new Vector2(0.5f, _rankInitialAnchorMax.y)
+                : _rankInitialAnchorMax;
+
+            _rankRectTransform.anchorMin = anchorMin;
+            _rankRectTransform.anchorMax = anchorMax;
+
+            Vector2 position = _rankRectTransform.anchoredPosition;
+            position.x = isPortrait ? 0f : _rankInitialPosition.x;
+            _rankRectTransform.anchoredPosition = position;
         }
 
         private void StartBlockMovement()
